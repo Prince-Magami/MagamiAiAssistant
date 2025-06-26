076349c4c77e884f4a957dfd961cdc3c600d1d53
Abubakar Muhammad Magami <magamiabu@gmail.com>
2025-06-26 16:34:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -74,8 +74,9 @@ def logout():
 def chat():
     return render_template('chat.html')
 
+import os
 
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
+
